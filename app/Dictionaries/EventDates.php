5148bde206c2db433c93b6656751f8ae4f7a8271
Kunzelman <mkunzelman@fmamfg.org>
2024-08-07 14:16:24 -0600

    modified EventDates custom dictionary per instructions.

--- app/Dictionaries/EventDates.php
+++ app/Dictionaries/EventDates.php
@@ -7,18 +7,14 @@ use Statamic\Facades\GlobalSet;
 
 class EventDates extends BasicDictionary
 {
+    protected string $valueKey = 'expo_date';
+    protected string $labelKey = 'expo_date';
 
     protected function getItems(): array
     {
         $globalSet = GlobalSet::findByHandle('expo_settings');
         $eventDates = $globalSet->inCurrentSite()->get('event_dates');
 
-        return collect($eventDates)->map(function ($eventDate) {
-            return [
-                'label' => $eventDate['expo_date'],
-                'value' => $eventDate['expo_date']
-            ];
-        })->all();
-
+        return $eventDates;
     }
 }
